75350bc62c18bfae1abff9539b310dad267a1bdf
PedroHercules <pedroherculesdantas@gmail.com>
2023-06-03 22:55:00 -0300

    refactor(register-service): added type response in execute method

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,11 +1,16 @@
 import { UsersRepository } from '@/repositories/users-repository'
 import { hash } from 'bcryptjs'
 import { UserAlreadyExistsError } from './errors/user-already-exists'
+import { User } from '@prisma/client'
 
 interface RegisterUseCaseProps {
   name: string
   email: string
   password: string
+}
+
+interface RegisterUseCaseResponse {
+  user: User
 }
 
 export class RegisterUseCase {
@@ -14,7 +19,11 @@ export class RegisterUseCase {
     this.usersRepository = usersRepository
   }
 
-  public async execute({ email, name, password }: RegisterUseCaseProps) {
+  public async execute({
+    email,
+    name,
+    password,
+  }: RegisterUseCaseProps): Promise<RegisterUseCaseResponse> {
     const password_hash = await hash(password, 6)
 
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
@@ -23,10 +32,14 @@ export class RegisterUseCase {
       throw new UserAlreadyExistsError()
     }
 
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password_hash,
     })
+
+    return {
+      user,
+    }
   }
 }
