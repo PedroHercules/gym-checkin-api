28e085a341a18fecf24a4c99fae8bb473323cbea
PedroHercules <pedroherculesdantas@gmail.com>
2023-06-04 16:32:41 -0300

    feat(check-in-repository): created in memory repository

---
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -0,0 +1,20 @@
+import { CheckIn, Prisma } from '@prisma/client'
+import { CheckInsRepository } from '../check-ins-repository'
+import { randomUUID } from 'node:crypto'
+
+export class InMemoryCheckInsRepository implements CheckInsRepository {
+  public checkIns: CheckIn[] = []
+  async create(data: Prisma.CheckInUncheckedCreateInput) {
+    const checkIn = {
+      id: randomUUID(),
+      user_id: data.user_id,
+      gym_id: data.gym_id,
+      validate_at: data.validate_at ? new Date(data.validate_at) : null,
+      created_at: new Date(),
+    }
+
+    this.checkIns.push(checkIn)
+
+    return checkIn
+  }
+}
