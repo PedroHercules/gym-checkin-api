f3ccd0e132480642ca291d31a4b8a10ee3ad63e5
PedroHercules <pedroherculesdantas@gmail.com>
2023-06-03 22:55:26 -0300

    test(register): added create user test

--- src/use-cases/register.test.ts
+++ src/use-cases/register.test.ts
@@ -1,5 +1,38 @@
-import { expect, test } from 'vitest'
+import { expect, describe, it } from 'vitest'
+import { RegisterUseCase } from './register'
+import { compare } from 'bcryptjs'
 
-test('check if works', () => {
-  expect(2 + 2).toEqual(4)
+const mockRepository = {
+  async findByEmail(email: string) {
+    console.log(email)
+    return null
+  },
+  async create(data: any) {
+    return {
+      id: 'user-1',
+      name: data.name,
+      email: data.email,
+      password_hash: data.password_hash,
+      created_at: new Date(),
+    }
+  },
+}
+
+describe('Register Use Case', () => {
+  it('should hash user password upon registration', async () => {
+    const registerUseCase = new RegisterUseCase(mockRepository)
+
+    const { user } = await registerUseCase.execute({
+      name: 'Jhon Doe',
+      email: 'jhondoe@email.com',
+      password: '123456',
+    })
+
+    const isPasswordCorrectlyHashed = await compare(
+      '123456',
+      user.password_hash,
+    )
+
+    expect(isPasswordCorrectlyHashed).toBe(true)
+  })
 })
