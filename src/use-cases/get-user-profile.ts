5643fefe659803bc386504e34dd5d410ffe6e3f4
PedroHercules <pedroherculesdantas@gmail.com>
2023-06-04 15:31:22 -0300

    feat(use-case): implemented get profile use case

---
+++ src/use-cases/get-user-profile.ts
@@ -0,0 +1,32 @@
+import { UsersRepository } from '@/repositories/users-repository'
+import { User } from '@prisma/client'
+import { ResourceNotFoundError } from './errors/resource-not-found'
+
+interface GetUserProfileUseCaseRequest {
+  userId: string
+}
+
+interface GetUserProfileUseCaseResponse {
+  user: User
+}
+
+export class GetUserProfileUseCase {
+  private usersRepository: UsersRepository
+  constructor(usersRespository: UsersRepository) {
+    this.usersRepository = usersRespository
+  }
+
+  async execute({
+    userId,
+  }: GetUserProfileUseCaseRequest): Promise<GetUserProfileUseCaseResponse> {
+    const user = await this.usersRepository.findById(userId)
+
+    if (!user) {
+      throw new ResourceNotFoundError()
+    }
+
+    return {
+      user,
+    }
+  }
+}
