38d8514e1d3d3736017a21e37bab29b4bc8d7742
PedroHercules <pedroherculesdantas@gmail.com>
2023-06-04 13:03:22 -0300

    feat(use-case): created authenticate user use case

---
+++ src/use-cases/authenticate.ts
@@ -0,0 +1,41 @@
+import { UsersRepository } from '@/repositories/users-repository'
+import { InvalidCredentiaslError } from './errors/invalid-credentials'
+import { compare } from 'bcryptjs'
+import { User } from '@prisma/client'
+
+interface AuthenticateUseCaseRequest {
+  email: string
+  password: string
+}
+
+interface AuthenticateUseCaseResponse {
+  user: User
+}
+
+export class AuthenticateUseCase {
+  private usersRepository: UsersRepository
+  constructor(usersRespository: UsersRepository) {
+    this.usersRepository = usersRespository
+  }
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateUseCaseRequest): Promise<AuthenticateUseCaseResponse> {
+    const user = await this.usersRepository.findByEmail(email)
+
+    if (!user) {
+      throw new InvalidCredentiaslError()
+    }
+
+    const doesPasswordMatches = await compare(password, user.password_hash)
+
+    if (!doesPasswordMatches) {
+      throw new InvalidCredentiaslError()
+    }
+
+    return {
+      user,
+    }
+  }
+}
