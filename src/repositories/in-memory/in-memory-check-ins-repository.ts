41110aa7e86a3c2405346adcedd68a10868a6839
PedroHercules <pedroherculesdantas@gmail.com>
2023-06-04 22:29:38 -0300

    feat(checkin-repository): implemented find check in user id by date

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -1,6 +1,7 @@
 import { CheckIn, Prisma } from '@prisma/client'
 import { CheckInsRepository } from '../check-ins-repository'
 import { randomUUID } from 'node:crypto'
+import dayjs from 'dayjs'
 
 export class InMemoryCheckInsRepository implements CheckInsRepository {
   public checkIns: CheckIn[] = []
@@ -16,5 +17,27 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     this.checkIns.push(checkIn)
 
     return checkIn
+  }
+
+  async findUserIdByDate(userId: string, date: Date) {
+    // Get the start of the day. Ex: 2023-06-04T00:00:00
+    const startOfTheDay = dayjs(date).startOf('date')
+
+    // Get the end of the day. Ex: 2023-06-04T23:59:59
+    const endOfTheDay = dayjs(date).endOf('date')
+
+    const checkInOnSameDate = this.checkIns.find((checkIn) => {
+      const checkInDate = dayjs(checkIn.created_at)
+      const isOnSameDate =
+        checkInDate.isAfter(startOfTheDay) && checkInDate.isBefore(endOfTheDay)
+
+      return checkIn.user_id === userId && isOnSameDate
+    })
+
+    if (!checkInOnSameDate) {
+      return null
+    }
+
+    return checkInOnSameDate
   }
 }
