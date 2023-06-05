d705ebf654c3806363c610a8546a15de027cd4a4
PedroHercules <pedroherculesdantas@gmail.com>
2023-06-04 23:06:37 -0300

    feat(repository): implemented gyms repository

---
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -0,0 +1,28 @@
+import { Gym, Prisma } from '@prisma/client'
+import { GymsRepository } from '../gyms-repository'
+import { randomUUID } from 'crypto'
+import { Decimal } from '@prisma/client/runtime/library'
+
+export class InMemoryGymsRepository implements GymsRepository {
+  public gyms: Gym[] = []
+
+  async create(data: Prisma.GymCreateInput) {
+    const gym: Gym = {
+      id: data.id || randomUUID(),
+      title: data.title,
+      description: data.description || null,
+      phone: data.phone || null,
+      latidude: new Decimal(data.latidude.toString()),
+      longitude: new Decimal(data.longitude.toString()),
+    }
+
+    this.gyms.push(gym)
+
+    return gym
+  }
+
+  async findById(id: string) {
+    const gym = this.gyms.find((gym) => gym.id === id) || null
+    return gym
+  }
+}
