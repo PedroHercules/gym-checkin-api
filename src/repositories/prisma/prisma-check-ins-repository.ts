b17da97f8f303e63908b52297d2b33a0ee41eb3c
PedroHercules <pedroherculesdantas@gmail.com>
2023-06-08 16:01:02 -0300

    feat(repositories): implemented gyms prisma repository

---
+++ src/repositories/prisma/prisma-check-ins-repository.ts
@@ -0,0 +1,80 @@
+import { Prisma, CheckIn } from '@prisma/client'
+import { CheckInsRepository } from '../check-ins-repository'
+import { prisma } from '@/lib/prisma'
+import dayjs from 'dayjs'
+
+export class PrismaCheckInsRepository implements CheckInsRepository {
+  async create(data: Prisma.CheckInUncheckedCreateInput) {
+    const checkIn = await prisma.checkIn.create({
+      data,
+    })
+
+    return checkIn
+  }
+  async findUserIdByDate(userId: string, date: Date) {
+    // Get the start of the day. Ex: 2023-06-04T00:00:00
+    const startOfTheDay = dayjs(date).startOf('date')
+
+    // Get the end of the day. Ex: 2023-06-04T23:59:59
+    const endOfTheDay = dayjs(date).endOf('date')
+
+    const checkIn = await prisma.checkIn.findFirst({
+      where: {
+        user_id: userId,
+        created_at: {
+          gte: startOfTheDay.toDate(), // Greater than or equal
+          lte: endOfTheDay.toDate(), // Lower than or equal
+        },
+      },
+    })
+
+    return checkIn
+  }
+
+  async findManyByUserId(userId: string, page: number) {
+    const checkIns = await prisma.checkIn.findMany({
+      where: {
+        user_id: userId,
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    })
+
+    return checkIns
+  }
+
+  async countByUserId(userId: string) {
+    const count = await prisma.checkIn.count({
+      where: {
+        user_id: userId,
+      },
+    })
+
+    return count
+  }
+
+  async findById(id: string) {
+    const checkIn = await prisma.checkIn.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    if (!checkIn) {
+      return null
+    }
+
+    return checkIn
+  }
+
+  async save(data: CheckIn) {
+    const checkIn = await prisma.checkIn.update({
+      where: {
+        id: data.id,
+      },
+      data,
+    })
+
+    return checkIn
+  }
+}
