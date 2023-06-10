f984529af2d8b359c0ae2a556049e8ac6129bd91
PedroHercules <pedroherculesdantas@gmail.com>
2023-06-10 13:57:04 -0300

    test(e2e): autheticate controller

--- src/http/controllers/authenticate-controller.test.ts
+++ src/http/controllers/authenticate-controller.test.ts
@@ -1,5 +1,30 @@
-import { expect, test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('ok', () => {
-  expect(1 + 1).toEqual(2)
+describe('Authenticate (E2E', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+  it('should be able to authenticate', async () => {
+    await request(app.server).post('/users').send({
+      name: 'Jhon Doe',
+      email: 'jhondoe@email.com',
+      password: '123456',
+    })
+
+    const response = await request(app.server).post('/sessions').send({
+      email: 'jhondoe@email.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    })
+  })
 })
