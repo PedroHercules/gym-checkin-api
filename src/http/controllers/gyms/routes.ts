5b86292c58afed46eeabf7edf9750732aa774ce9
PedroHercules <pedroherculesdantas@gmail.com>
2023-06-10 17:15:47 -0300

    feat(gyms-controller): implemented search and nearby

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,9 +1,13 @@
 import { FastifyInstance } from 'fastify'
 import { verifyJwt } from '@/http/middlewares/verify-jwt'
 import { create } from './create-controller'
+import { search } from './search-controller'
+import { nearby } from './nearby-controller'
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJwt)
 
   app.post('/gyms', create)
+  app.get('/gyms/search', search)
+  app.get('/gyms/nearby', nearby)
 }
