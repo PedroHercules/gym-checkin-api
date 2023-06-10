3783c33674cd62c997f9e4ebf04ea5eadeae4d87
PedroHercules <pedroherculesdantas@gmail.com>
2023-06-10 13:53:00 -0300

    test(e2e): register controller test

--- src/http/controllers/register-controller.test.ts
+++ src/http/controllers/register-controller.test.ts
@@ -1,5 +1,22 @@
-import { expect, test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('ok', () => {
-  expect(1 + 1).toEqual(2)
+describe('Register (E2E', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+  it('should be able to register', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'Jhon Doe',
+      email: 'jhondoe@email.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
 })
