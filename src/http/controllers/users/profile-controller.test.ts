8934a8548b7b6a6dd7f93290d99b1527eb549c03
PedroHercules <pedroherculesdantas@gmail.com>
2023-06-10 18:51:38 -0300

    test(e2e): refactor get user token on profile test

--- src/http/controllers/users/profile-controller.test.ts
+++ src/http/controllers/users/profile-controller.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest'
 import { app } from '@/app'
 import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
 
 describe('Profile (E2E)', () => {
   beforeAll(async () => {
@@ -11,18 +12,7 @@ describe('Profile (E2E)', () => {
     await app.close()
   })
   it('should be able to get user profile', async () => {
-    await request(app.server).post('/users').send({
-      name: 'Jhon Doe',
-      email: 'jhondoe@email.com',
-      password: '123456',
-    })
-
-    const authResponse = await request(app.server).post('/sessions').send({
-      email: 'jhondoe@email.com',
-      password: '123456',
-    })
-
-    const { token } = authResponse.body
+    const { token } = await createAndAuthenticateUser(app)
 
     const profileResponse = await request(app.server)
       .get('/me')
