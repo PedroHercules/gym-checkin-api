7b4dacc9151aeec048b6e2e8da1b79406d93e72a
PedroHercules <pedroherculesdantas@gmail.com>
2023-06-10 18:51:57 -0300

    test(e2e): implemented create gym test

---
+++ src/http/controllers/gyms/create-controller.test.ts
@@ -0,0 +1,31 @@
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+
+describe('Create Gym (E2E)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create a gym', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const response = await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'JS Gym',
+        description: 'Some description',
+        phone: '898089890',
+        latitude: 40.69754,
+        longitude: -74.3093378,
+      })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
