e99746d806d62b571a0d31fec95b077dff4e208d
PedroHercules <pedroherculesdantas@gmail.com>
2023-06-10 16:56:16 -0300

    refactor(users): renamed to user routes

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -4,7 +4,7 @@ import { authenticate } from './authenticate-controller'
 import { profile } from './profile-controller'
 import { verifyJwt } from '@/http/middlewares/verify-jwt'
 
-export async function appRoutes(app: FastifyInstance) {
+export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
 
